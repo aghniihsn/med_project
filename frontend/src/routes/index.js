9fc9f86dd8a9525c0dcf0e6a97d9df2b50b3c80e
mrestuab <mrestuab54@gmail.com>
2024-12-15 19:29:56 +0700

    add reminder

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,6 +3,8 @@ import { Routes as BaseRoutes, Route, Navigate} from "react-router-dom";
 import Layout from "../components/Layout";
 import Login from "./Login";
 import Register from "./Register";
+import Dashboard from "./Dashboard";
+import Add_Reminder from "./Add_Reminder";
 
 export default function Routes() {
     return (
@@ -21,6 +23,18 @@ export default function Routes() {
                   <Register />
               }
             />
+            <Route
+              path="/dashboard"
+              element={
+                  <Dashboard />
+                }
+             />
+             <Route
+              path="/add"
+              element={
+                  <Add_Reminder />
+                }
+             />
           </Route>  
         </BaseRoutes>
       );
