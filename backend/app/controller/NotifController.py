403bc862e4e13b37c6deeaa2887ec137437ea47e
mrestuab <mrestuab54@gmail.com>
2025-01-10 08:30:58 +0700

    notif wa

--- backend/app/controller/NotifController.py
+++ backend/app/controller/NotifController.py
@@ -17,7 +17,7 @@ def send_message(token, target, message):
     try:
         response = requests.post(url, json=payload, headers=headers)
         response.raise_for_status()
-        return response.json() 
+        return response.json()  # Mengembalikan respons dari API
     except requests.exceptions.RequestException as e:
         print(f"Error sending message: {str(e)}")
         return {"error": str(e)}
