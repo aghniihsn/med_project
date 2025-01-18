c077c07252c17cc6d523acc9d662c8f98b568ba4
aghni <aghniihsn.gmail.com>
2025-01-18 20:26:51 +0700

    name in dashboard

--- frontend/src/routes/Dashboard/index.js
+++ frontend/src/routes/Dashboard/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import { Layout, Row, Col } from "antd";
 import { PlusCircleOutlined, ScheduleOutlined, FileTextOutlined } from "@ant-design/icons";
+import cookie from "../../core/helpers/cookie";
 
 const { Content} = Layout;
 
 function Dashboard() {
+  const cookieUser = cookie.get("user");
+  const userName = cookieUser ? JSON.parse(cookieUser).name : "Guest";
   const navigate = useNavigate();
 
   function handleClickAdd(){
@@ -20,7 +23,7 @@ function Dashboard() {
     <Layout style={{ minHeight: "100vh" }}>
       <Content style={{ padding: "50px", backgroundColor: "#F2F9FF" }}>
         <div style={{ textAlign: "center", marginBottom: "50px" }}>
-          <h1 level={2}>Hello, Name</h1>
+          <h1 level={2}>Hello, {userName}</h1>
           <p>
             Jangan khawatir soal waktu, cukup atur jadwalmu dan biarkan kami
             mengingatkanmu!
